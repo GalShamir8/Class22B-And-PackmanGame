da62c36160a39d5e733e943002f7f1a1230d8b19
Gal Shamir <galshamir8@gmail.com>
2022-03-21 20:48:30 +0200

    initializer for game grid, add const/final game attributes

--- app/src/main/java/controllers/GameManager.java
+++ app/src/main/java/controllers/GameManager.java
@@ -8,7 +8,16 @@ public class GameManager {
     public static final int COLUMNS = 3;
     public static final int ROWS = 5;
 
-    private ImageView[][] gameGrid = new ImageView[COLUMNS][ROWS];
+    public static final int[] PLAYER_START_INDEX = {ROWS, (int)COLUMNS/2};
+    public static final int[] RIVAL_START_INDEX = {0, ROWS};
+
+    private static final int LIVES = 3;
+
+    private ImageView[][] gameGrid = new ImageView[ROWS][COLUMNS];
+
+    private int score = 0;
+    private int lives = LIVES;
+
 
     /**
      * default constructor
@@ -18,9 +27,17 @@ public class GameManager {
     }
 
     private void initGrid() {
-        for (int colIndex = 0; colIndex < COLUMNS; colIndex++) {
-            for (int rowIndex = 0; rowIndex < ROWS; rowIndex++) {
-                gameGrid[rowIndex][colIndex].setImageResource(R.drawable.game_background);
+        for (int rowIndex = 0; rowIndex < ROWS; rowIndex++) {
+            for (int colIndex = 0; colIndex < COLUMNS; colIndex++) {
+                if(rowIndex == PLAYER_START_INDEX[0] && colIndex == PLAYER_START_INDEX[1]){
+                    // TODO: 21/03/2022 Add player img 
+                    gameGrid[rowIndex][colIndex].setImageResource(R.drawable.game_background);
+                }else if(rowIndex == RIVAL_START_INDEX[0] && colIndex == RIVAL_START_INDEX[1]){
+                    // TODO: 21/03/2022 Add rival img
+                    gameGrid[rowIndex][colIndex].setImageResource(R.drawable.game_background);
+                }else {
+                    gameGrid[rowIndex][colIndex].setImageResource(R.drawable.game_background);
+                }
             }
         }
     }
