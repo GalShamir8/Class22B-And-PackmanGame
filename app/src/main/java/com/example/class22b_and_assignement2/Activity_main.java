3f3120c7ddbfa90a880863b7f18aa22fcb35a1bd
Gal Shamir <galshamir8@gmail.com>
2022-03-21 19:50:08 +0200

    Initial commit - start build view scaffold

---
+++ app/src/main/java/com/example/class22b_and_assignement2/Activity_main.java
@@ -0,0 +1,14 @@
+package com.example.class22b_and_assignement2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
