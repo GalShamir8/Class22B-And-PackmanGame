143f8126233d411073097eecc69502ae683d1a46
Gal Shamir <galshamir8@gmail.com>
2022-04-21 19:47:21 +0300

    add load fragments for users to display it dynamically

--- app/src/main/java/com/example/class22b_and_assignement2/activities/TopTen_Activity.java
+++ app/src/main/java/com/example/class22b_and_assignement2/activities/TopTen_Activity.java
@@ -1,16 +1,28 @@
 package com.example.class22b_and_assignement2.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import com.example.class22b_and_assignement2.R;
+import com.example.class22b_and_assignement2.fragments.UsersFragment;
 
 public class TopTen_Activity extends AppCompatActivity {
+    private Bundle data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topten);
+        data = getIntent().getExtras();
+        loadFragmentUsers();
+    }
+
+    private void loadFragmentUsers() {
+        Fragment usersFragment = new UsersFragment();
+        usersFragment.setArguments(data);
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.topTen_scroll, usersFragment).commit();
     }
 }
